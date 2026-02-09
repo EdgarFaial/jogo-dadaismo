bd65d86ebba645e7e98663ec72b2a4ade2ae67f3
EdgarFaial <edgarofaialm@gmail.com>
2026-02-09 14:02:38 -0300

    Delete services directory

--- services/geminiService.ts
+++
@@ -1,4 +0,0 @@
-
-// This service was intentionally removed as per user request to use predefined responses.
-export const getDadaistQuote = async () => "Dada não significa nada.";
-export const getGeminiVerdict = async () => ({ allow: true, reason: "O Vácuo aceitou seu pedido." });
